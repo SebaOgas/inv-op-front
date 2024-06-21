9f1daaf36fdfa1f64b0b999a7fb336093adda876
fpalladino <fpalladino@conexia.com>
2024-06-21 10:53:51 -0300

    WIP: Orden de compra

--- src/routes/OrdenDeCompra/+page.svelte
+++ src/routes/OrdenDeCompra/+page.svelte
@@ -19,6 +19,7 @@
 	}
 
 	let purchaseOrders: DTOPurchaseOrder[] = [];
+	let showClosed = false;
 
 	onMount(() => {
 		getPurchaseOrders();
@@ -44,11 +45,28 @@
 
 <div class="container-fluid">
 	<h2 class="position-fixed">Listado de ordenes de compra</h2>
-	<button class="position-fixed end-0 m-2"
-		on:click={() => {
-			redir('/OrdenDeCompra/RegistrarOrdenDeCompra');
-		}}>Generar Orden de Compra</button	
-	>
+</div>
+<div class="position-fixed end-0">
+	<div class="container-fluid">
+		<button class=""
+			on:click={() => {
+				redir('/OrdenDeCompra/RegistrarOrdenDeCompra');
+			}}>Generar Orden de Compra</button	
+		>
+		<button class=""
+			on:click={() => {
+				redir('/OrdenDeCompra/RegistrarOrdenDeCompra');
+			}}>Calcular ordenes de Compra</button	
+		>
+	</div>
+	<div class="container-fluid end-0">
+		<label>
+			<input type="checkbox" class="" bind:checked={showClosed} />
+			Mostrar cerrados
+		</label>
+	</div>
+</div>
+<div class="i-flex">
     <br>
     <br>
 	<table class="table table-hover">
@@ -56,32 +74,34 @@
 			<tr>
 				<td class="col-sm-1">Fecha de la orden</td>
 				<td class="col-sm-3">Proveedor</td>
-				<td class="col-sm-3">Producto</td>
+				<td class="col-sm-2">Producto</td>
 				<td class="col-sm-2">Cantidad pedida</td>
 				<td class="col-sm-2">Estado de la orden</td>
 				<!-- <td>Fecha</td> -->
-				<td class="col-sm-1">Accion</td>
+				<td class="col-sm-2">Accion</td>
 			</tr>
 		</thead>
 
 		<tbody>
 			{#if purchaseOrders.length > 0}
 				{#each purchaseOrders as purchaseOrder (purchaseOrder.purchaseOrderId)}
-					<tr>
-						<td>{format(purchaseOrder.purchaseOrderDate, 'dd/MM/yyyy')}</td>
-						<!-- <td>{sale.saleId}</td> -->
-						<td>{purchaseOrder.supplierName}</td>
-						<td>{purchaseOrder.productName}</td>
-						<td>{purchaseOrder.orderQuantity}</td>
-						<td>{parseStatus(purchaseOrder.purchaseOrderStatus)}</td>
-						<td>
-							<!-- <a href="/OrdenDeCompra/{purchaseOrder.purchaseOrderId}">Ver</a> -->
-							<!-- <button on:click={() => redir(`/OrdenDeCompra{purchaseOrder.purchaseOrderId}`)}>Modificar</button> -->
-							<button on:click={() => redir(`/OrdenDeCompra/${purchaseOrder.purchaseOrderId}`)}>Ver</button>
-							<!-- <button on:click={() => redir(`/OrdenDeCompra/${purchaseOrder.purchaseOrderId}`)} style="background-color: green; color: aliceblue;">Completar</button> -->
-							<button on:click={() => closePurchaseOrder(purchaseOrder.purchaseOrderId)} style="background-color: green; color: aliceblue;">Completar</button>
-						</td>
-					</tr>
+					{#if purchaseOrder.purchaseOrderStatus === PurchaseOrderStatusEnum.OPEN || showClosed }
+						<tr>
+							<td>{format(purchaseOrder.purchaseOrderDate, 'dd/MM/yyyy')}</td>
+							<!-- <td>{sale.saleId}</td> -->
+							<td>{purchaseOrder.supplierName}</td>
+							<td>{purchaseOrder.productName}</td>
+							<td>{purchaseOrder.orderQuantity}</td>
+							<td>{parseStatus(purchaseOrder.purchaseOrderStatus)}</td>
+							<td>
+								<!-- <a href="/OrdenDeCompra/{purchaseOrder.purchaseOrderId}">Ver</a> -->
+								<!-- <button on:click={() => redir(`/OrdenDeCompra{purchaseOrder.purchaseOrderId}`)}>Modificar</button> -->
+								<button on:click={() => redir(`/OrdenDeCompra/${purchaseOrder.purchaseOrderId}`)}>Ver</button>
+								<!-- <button on:click={() => redir(`/OrdenDeCompra/${purchaseOrder.purchaseOrderId}`)} style="background-color: green; color: aliceblue;">Completar</button> -->
+								<button on:click={() => closePurchaseOrder(purchaseOrder.purchaseOrderId)} style="background-color: green; color: aliceblue;">Completar</button>
+							</td>
+						</tr>
+					{/if}
 				{/each}
 			{:else}
 				<h3>No hay ordenes de compra registradas</h3>
--- src/routes/Ventas/RegistrarVenta/+page.svelte
+++ src/routes/Ventas/RegistrarVenta/+page.svelte
@@ -6,10 +6,16 @@
     import { onMount } from 'svelte';
     import { VentaService } from '../VentaService';
 	import type { ActionData } from './$types';
+	import type DTONewSale from '../DTONewSale';
 
     let productos : DTOProduct[] = [];
 
     export let form: ActionData;
+
+    let newSale : DTONewSale = {
+        customerName: "",
+        quantity: 0
+    }
 
     onMount(() => {
 		getProducts();
@@ -33,60 +39,52 @@
         return form.formError;
     }
 
+    async function createSale() {
+        try {
+            await VentaService.sale.create(newSale);
+            alert('Orden de compra creada correctamente');
+            if (typeof window !== 'undefined') {
+                window.location.href = "/Ventas";
+            }
+        } catch (error) {
+            console.error("Error creando la orden de compra:", error);
+            alert('Error al crear la orden de compra: ' + error.message);
+        }
+    }
+
 </script>
 
 
 <div class="container-fluid">
-    {#if form !== null && !form?.success}
-    <div class="alert alert-danger" role="alert">
-        {getErrorMessage()}
-    </div>
-    {/if}
-    {#if form !== null && form?.success}
-        <div class="alert alert-success" role="alert">
-            La venta se guardó con éxito.
-        </div>
-    {/if}
     <h1>Registrar Venta</h1>
-    <form method="POST" action="?/createSale">
+    <form method="POST" on:submit|preventDefault={createSale}>
         <div class="mb-3 row">
             <label for="customerName" class="col-sm-4 col-form-label">Nombre del cliente:</label>
             <div class="col-sm-4">
-                
-                {#if form?.formData.customerName !== undefined && !form?.success}
-                    <input type="text" class="form-control" name="customerName" value={form?.formData.customerName} required>
-                {:else}
-                    <input type="text" class="form-control" name="customerName" required>
-                {/if}
-                </div>
+                <input type="text" class="form-control" name="customerName" bind:value={newSale.customerName} required>
             </div>
-            <div class="mb-3 row">
-                <label for="productName" class="col-sm-4 col-form-label">Producto</label>
-                <div class="col-sm-4">
-                <select name="productId"  class="form-select" aria-label="Default select example" required>
+        </div>
+        <div class="mb-3 row">
+            <label for="productName" class="col-sm-4 col-form-label">Producto</label>
+            <div class="col-sm-4">
+                <select name="productId"  class="form-select" aria-label="Default select example" bind:value={newSale.productId} required>
                     <option selected>Seleccione un producto</option>                    
                     {#each productos as product }
-                        <option value="{product.productId}">{product.productName}</option>
+                        <option value={product.productId}>{product.productName}</option>
                     {/each}
-
                 </select>
-                </div>
             </div>
-            <div class="mb-3 row">
-                <label for="quantity" class="col-sm-4 col-form-label">Cantidad pedida</label>
-                <div class="col-sm-4">
-                {#if form?.formData.quantity !== undefined && !form?.success}
-                    <input type="number" class="form-control" name="quantity" min=0 value={form?.formData.quantity}>
-                {:else}
-                <input type="number" class="form-control" name="quantity" min=0 required>
-                {/if}
-                </div>
+        </div>
+        <div class="mb-3 row">
+            <label for="quantity" class="col-sm-4 col-form-label">Cantidad pedida</label>
+            <div class="col-sm-4">
+                <input type="number" class="form-control" name="quantity" min=0 bind:value={newSale.quantity} required>
             </div>
-            <dir class="position-fixed end-0 m-2">
-                <button type="submit">Guardar</button>
-                <button type="button" on:click={() => {redir("/Ventas")}}>Cancelar</button>
-            </dir>
+        </div>
+        <div class="position-fixed end-0 m-2">
+            <button type="submit">Guardar</button>
+            <button type="button" on:click={() => {redir("/Ventas")}}>Cancelar</button>
+        </div>
     </form>
-    
 </div>
 
